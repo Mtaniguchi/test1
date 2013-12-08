48bb43ea6bffefe77f9608b4c54e22e1d78a5b7a
Mtaniguchi <ba08004@cc.it-hiroshima.co.jp>
2013-12-09 07:04:00 +0900

    アダプターを追加し修正

--- List/src/com/sample/list/MainActivity.java
+++ List/src/com/sample/list/MainActivity.java
@@ -9,17 +9,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 public class MainActivity extends Activity {
   ArrayAdapter<String> adapter;
-  private TextView textView;
+  static List<String> items = new ArrayList<String>();
+  
+  private int count2;
+  
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        count2 = 0;
 
         //ListViewオブジェクトの取得
         ListView listView=(ListView)findViewById(R.id.list_view);
+        
+        /*
+        List<Map<String, String>> retDataList = new ArrayList<Map<String, String>>();
 
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("title", "一行目");
+        data.put("comment", "１１１");
+        data.put("note", "aaa");
+        retDataList.add(data);
+        
+        data = new HashMap<String, String>();
+        data.put("title", "二行目");
+        data.put("comment", "２２２");
+        data.put("note", "bbb");
+        retDataList.add(data);
+        
+        SimpleAdapter adapter2 = new SimpleAdapter(this, retDataList,
+        		R.layout.row, new String[] { "title", "comment" ,"note"},
+        	    new int[] {R.id.textView1, R.id.textView2 , R.id.textView3});
+        
+        listView.setAdapter(adapter2);
+        */
+        
         //ArrayAdapterオブジェクト生成
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 
@@ -35,11 +69,13 @@ public class MainActivity extends Activity {
         //要素追加
         addStringData();
         
+        /*
         TextView textView1=(TextView)findViewById(R.id.textView1);
         // EditText editText1=(EditText)findViewById(R.id.editText1);
         // Editable input=editText1.getText();
         //textView1.setText(input);
         textView1.setText("hei");
+        */
         
       }
     });
@@ -50,13 +86,36 @@ public class MainActivity extends Activity {
     //要素追加処理
     private void addStringData(){
 
+    	TextView textView1=(TextView)findViewById(R.id.textView1);
+    	
+    	count2++;
+    	EditText edit=(EditText)findViewById(R.id.edit_text);
+    	
+    	 ListView listView=(ListView)findViewById(R.id.list_view);
+    	 items.add(edit.getText().toString());
+    	 
+    	 
+    	    adapter = 
+    	      new ArrayAdapter<String>(
+    	        this,
+    	        R.layout.row,
+    	        R.id.textView1,
+    	        items);
+    	    
+    	    listView.setAdapter(adapter);
+    
+    	
+    /*
       //EditTextオブジェクト取得
       EditText edit=(EditText)findViewById(R.id.edit_text);
       
       
+      count2++;
       
-
       //EditText(テキスト)を取得し、アダプタに追加
       adapter.add(edit.getText().toString());
+      adapter.add((count2)+"hey");
+      
+      */
     }
 }
